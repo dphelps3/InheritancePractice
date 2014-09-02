d98a3601140ae696864000079fc62c69f3c17744
David <David@192.168.12.101>
2014-09-02 13:12:54 -0500

    Homework Task #1

--- src/myconcrete/Employee.java
+++ src/myconcrete/Employee.java
@@ -17,102 +17,143 @@ public class Employee {
     
                     empDept; // Holds employee company info
     
-    private int empIDnumber,
+    private int empIDNumber,
                 empZip,
                 empPhone; // Holds numeric employee info
     
-    // Methods that will store the local variables as public arguments
     
+// Methods that will store the local variables as public arguments
+     
+     // set and get empFirstName
+     public void setEmpFirstName(String employeeFirstName)
+     {
+         empFirstName = employeeFirstName;
+     }
+     
+     public String getEmpFirstName()
+     {
+         return empFirstName;
+     }
     
-    public String getEmpFirstName() {
-        return empFirstName;
-    }
-
-    // Links private string empFirstName to public variable employeeFirstName
-    public void setEmpFirstName(String empFirstName) {
-        
-        empFirstName = employeeFirstName;
-    }
-
-    public String getEmpLastName() {
-        return empLastName;
-    }
-
-    public void setEmpLastName(String empLastName) {
-        this.empLastName = empLastName;
-    }
-
-    public String getEmpBirthdate() {
-        return empBirthdate;
-    }
-
-    public void setEmpBirthdate(String empBirthdate) {
-        this.empBirthdate = empBirthdate;
-    }
-
-    public String getEmpAddress() {
-        return empAddress;
-    }
-
-    public void setEmpAddress(String empAddress) {
-        this.empAddress = empAddress;
-    }
-
-    public String getEmpCity() {
-        return empCity;
-    }
-
-    public void setEmpCity(String empCity) {
-        this.empCity = empCity;
-    }
-
-    public String getEmpState() {
-        return empState;
-    }
-
-    public void setEmpState(String empState) {
-        this.empState = empState;
-    }
-
-    public String getEmpEmail() {
-        return empEmail;
-    }
-
-    public void setEmpEmail(String empEmail) {
-        this.empEmail = empEmail;
-    }
-
-    public String getEmpDept() {
-        return empDept;
-    }
-
-    public void setEmpDept(String empDept) {
-        this.empDept = empDept;
-    }
-
-    public int getEmpIDnumber() {
-        return empIDnumber;
-    }
-
-    public void setEmpIDnumber(int empIDnumber) {
-        this.empIDnumber = empIDnumber;
-    }
-
-    public int getEmpZip() {
-        return empZip;
-    }
-
-    public void setEmpZip(int empZip) {
-        this.empZip = empZip;
-    }
-
-    public int getEmpPhone() {
-        return empPhone;
-    }
-
-    public void setEmpPhone(int empPhone) {
-        this.empPhone = empPhone;
-    }
-    
+     // set and get empLastName
+     public void setEmpLastName(String employeeLastName)
+     {
+         empLastName = employeeLastName;
+     }
+     
+     public String getEmpLastName()
+     {
+         return empLastName;
+     }
+     
+     // set and get empBirthdate
+     public void setEmpBirthdate(String employeeBirthdate)
+     {
+         empBirthdate = employeeBirthdate;
+     }
+     
+     public String getEmpBirthdate()
+     {
+         return empBirthdate;
+     }
+     
+     // set and get empAddress
+     public void setEmpAddress(String employeeAddress)
+     {
+         empAddress = employeeAddress;
+     }
+     
+     public String getEmpAddress()
+     {
+         return empAddress;
+     }
+     
+     // set and get empCity
+     public void setEmpCity(String employeeCity)
+     {
+         empCity = employeeCity;
+     }
+     
+     public String getEmpCity()
+     {
+         return empCity;
+     }
+     
+     // set and get empState
+     public void setEmpState(String employeeState)
+     {
+         empState = employeeState;
+     }
+     
+     public String getEmpState()
+     {
+         return empState;
+     }
+     
+     // set and get empEmail
+     public void setEmpEmail(String employeeEmail)
+     {
+         empEmail = employeeEmail;
+     }
+     
+     public String getEmpEmail()
+     {
+         return empEmail;
+     }
+     
+     // set and get empDept
+     public void setEmpDept(String employeeDept)
+     {
+         empEmail = employeeDept;
+     }
+     
+     public String getEmpDept()
+     {
+         return empDept;
+     }
+     
+     // set and get empIDNumber
+     public void setEmpIDNumber(int employeeIDNumber)
+     {
+         empIDNumber = employeeIDNumber;
+     }
+     
+     public int getEmpIDNumber()
+     {
+         return empIDNumber;
+     }
+     
+     // set and get empZip
+     public void setZip(int employeeZip)
+     {
+         empZip = employeeZip;
+     }
+     
+     public String getEmpDept()
+     {
+         return empDept;
+     }
+     
+     // set and get empDept
+     public void setEmpDept(String employeeDept)
+     {
+         empEmail = employeeDept;
+     }
+     
+     public String getEmpDept()
+     {
+         return empDept;
+     }
+     
+     // set and get empDept
+     public void setEmpDept(String employeeDept)
+     {
+         empEmail = employeeDept;
+     }
+     
+     public String getEmpDept()
+     {
+         return empDept;
+     }
     
 }
--- src/myconcrete/EmployeeInput.java
+++ src/myconcrete/EmployeeInput.java
@@ -5,12 +5,15 @@ import javax.swing.JOptionPane;
 
 public class EmployeeInput {
     
+    public static void main(String[] args) {
+        
+    
     // Public variables that will link with the private variables of
     // the Employee class
     
     String employeeInput; // For user input
     
-    public String employeeFirstName,
+    String employeeFirstName,
                     employeeLastName,
                     employeeBirthdate,
                     employeeAddress,
@@ -20,13 +23,74 @@ public class EmployeeInput {
     
                     employeeDept; // Holds employee company info
     
-    public int employeeIDnumber,
+    int employeeIDNumber,
                 employeeZip,
                 employeePhone; // Holds numeric employee info
     
+    // Create an Employee object
+    Employee e = new Employee();
+    
+    // Get the user's company ID number
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your company ID number: ");
+    employeeIDNumber = Integer.parseInt(employeeInput);
+
     // Get the user's first name
     employeeInput = JOptionPane.showInputDialog(null, 
             "Enter your first name: ");
     employeeFirstName = employeeInput;
     
+    // Get the user's last name
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your last name: ");
+    employeeLastName = employeeInput;
+    
+    // Get the user's birthdate
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your birthdate: ");
+    employeeBirthdate = employeeInput;
+    
+    // Get the user's street address
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your street address: ");
+    employeeAddress = employeeInput;
+    
+    // Get the user's city
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your city: ");
+    employeeCity = employeeInput;
+    
+    // Get the user's state
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your state: ");
+    employeeState = employeeInput;
+    
+    // Get the user's zip code
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your zip code: ");
+    employeeZip = Integer.parseInt(employeeInput);
+    
+    // Get the user's phone number
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your phone number: ");
+    employeePhone = Integer.parseInt(employeeInput);
+    
+    // Get the user's email address
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your email address: ");
+    employeeEmail = employeeInput;
+    
+    // Get the user's department
+    employeeInput = JOptionPane.showInputDialog(null, 
+            "Enter your department: ");
+    employeeDept = employeeInput;
+    
+    
+    // Output information, getting the moethods from the Employee class
+    System.out.print("Name: " + e.getEmpFirstName() + "" + e.getEmpLastName());
+    
+    
+    
+    }
 }
+
