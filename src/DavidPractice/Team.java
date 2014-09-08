d3dfbfbeca542060faf782d85df8caf3bf03506f
David <David@192.168.12.101>
2014-09-07 19:02:19 -0500

    Homework Task #1

---
+++ src/DavidPractice/Team.java
@@ -0,0 +1,20 @@
+
+package DavidPractice;
+
+/**
+ *
+ * @author David
+ */
+public interface Team {
+    
+    public abstract void coach();
+    
+    public abstract String getTeamName();
+    
+    public abstract void setTeamName(String teamName);
+    
+    public abstract int getRank();
+    
+    public abstract void setRank(int rank);
+    
+}
